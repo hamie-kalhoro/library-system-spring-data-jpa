677bfb854abf2f596cb9868d95e1176f0d6afde7
Hamid Ali Kalhoro <hamidKalhoro.in@gmail.com>
2025-04-12 20:10:55 +0500

    initial commit

---
+++ src/main/java/com/hamidz/jpaCourse/LibrarySystem.java
@@ -0,0 +1,13 @@
+package com.hamidz.jpaCourse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibrarySystem {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibrarySystem.class, args);
+	}
+
+}
