9769d8867da2bb5dd55bfc19c4cb6da1e4cd3051
Hamid Ali Kalhoro <hamidKalhoro.in@gmail.com>
2025-04-19 16:46:41 +0500

    connecting Loan class with database for data operations

---
+++ src/main/java/com/hamidz/jpaCourse/repository/LoanRepository.java
@@ -0,0 +1,8 @@
+package com.hamidz.jpaCourse.repository;
+
+import com.hamidz.jpaCourse.entity.Loan;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LoanRepository extends JpaRepository<Loan, Long> {
+
+}
